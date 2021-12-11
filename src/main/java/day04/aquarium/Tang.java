a4327c8ab0df7c4c0105d8d370f99888b26f2feb
ht0m <hartht@gmail.com>
2021-12-11 20:50:14 +0100

    Formatted

--- src/main/java/day04/aquarium/Tang.java
+++ src/main/java/day04/aquarium/Tang.java
@@ -3,7 +3,7 @@ package day04.aquarium;
 public class Tang extends Fish{
     public Tang(String name, int weight, String color) {
         super(name, weight, color);
-        this.setMemoryLoss(true);
+        setMemoryLoss(true);
     }
 
     @Override
