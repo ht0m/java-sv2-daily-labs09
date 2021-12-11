473e4080f0e75aabeed6ec022fb81bc8774c3b82
ht0m <hartht@gmail.com>
2021-12-11 19:49:49 +0100

    Stringlist - shortest

--- src/main/java/day02/StringLists.java
+++ src/main/java/day02/StringLists.java
@@ -6,9 +6,7 @@ import java.util.List;
 public class StringLists {
     public List<String> shortestWords(List<String> words) {
         List<String> resultList = new ArrayList<>();
-
         int minLength = Integer.MAX_VALUE;
-
         for (String actual : words) {
             if (actual.length() < minLength) {
                 resultList.clear();
@@ -18,7 +16,6 @@ public class StringLists {
                 resultList.add(actual);
             }
         }
-
         return resultList;
     }
 }
